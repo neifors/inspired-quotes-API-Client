03b29162f742edb7cd7ef1e64959efdce64bbffe
isabel <isargp@gmail.com>
2022-03-01 11:13:54 +0000

    client working with a random quote

--- client/assets/script.js
+++ client/assets/script.js
@@ -1,10 +1,17 @@
 const form = document.querySelector('form')
 
-form.addEventListener('submit', getRandomQuote())
+form.addEventListener('submit', getRandomQuote)
 
 function getRandomQuote(e) {
   e.preventDefault()
-  fetch('http://localhost:3000/quotes')
-    .then(resp => resp.text)
-    .then(document.getElementById('text').innerHTML = quote)
+  fetch('http://localhost:3000/quotes/random')
+    .then(function(response) {
+        return response.text()
+    })
+    .then( function(data) {
+        const h1 = document.getElementById("text")
+        h1.textContent = data
+        console.log(data)
+    })
+  
 }
--- server/app.js
+++ server/app.js
@@ -29,22 +29,19 @@ app.use(cors());
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-// To do: Create a route for retrieving all quotes
 app.get('/quotes/', (req, res) =>  res.send(quotes) )
 
-// To do: Create a route for retrieving a random quote
 app.get('/quotes/random/', (req, res) => res.send(getRandomQuote()))
 
-// To do: Refine this route and add handling for out-of-range IDs
 app.get('/quotes/:id', (req, res) => {
   if (req.params.id >= quotes.length || req.params.id < 1) {
-    res.send(`ERROR: ID out of range. Please try again with an ID between 1 and ${quotes.length}`)
+    res.send({"error":`ID out of range. Please try again with an ID between 1 and ${quotes.length}`})
   } else {
     res.send(quotes[req.params.id - 1])
   }
 })
 
-// To do: Get the server running
+
 app.listen( PORT, () => {
   console.log(`\nExpress departing now from port ${PORT}!\n`)
 } );
